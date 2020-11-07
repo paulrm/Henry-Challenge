48eda7e2f98b31fb16ab8ff5698fd5afa0382c3a
delfinalago <delfinalago@gmail.com>
2020-11-07 20:05:55 -0300

    ArregloSearchBarV1.1

--- client/src/Component/SearchBar/SearchBar.js
+++ client/src/Component/SearchBar/SearchBar.js
@@ -4,14 +4,23 @@ import { useDispatch} from 'react-redux';
 import {Button} from 'react-bootstrap';
 import { ImSearch } from "react-icons/im";
 
+let value = "";
 
 const SerchBar = () => {
 
     // const dispatch = useDispatch();
 
+    const handlerButton = (e) => {
+        console.log("VALOR 2 =", value)
+    }
+
     const handlerSearch = (e) => {
         //setSerch(e.target.value);
-        let value = e.target.value;
+        // let elemento = document.getElementById("Search");
+        value = e.target.value;
+        console.log("VALOR ? =", value)
+        // console.log("ELEMENTO =", elemento)
+        
         // if (value !== '') {
         //     dispatch(mostrarBusqueda(e.target.value));
         // }else {
@@ -22,8 +31,8 @@ const SerchBar = () => {
     return (
 
         <Link to={'/search'} >
-            <Button type="submit" variant="light" ><ImSearch/></Button>
-            <input  type="text" placeholder="Buscar productos"
+            <Button type="submit" variant="light" onClick={handlerButton}  ><ImSearch/></Button>
+            <input name="Search" id="Search" type="text" placeholder="Buscar productos"
                 onChange={handlerSearch} />
         </Link>
 
